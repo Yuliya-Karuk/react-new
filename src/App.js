5566b75dd735770251c1557f982ef8c6634920b3
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-12-11 21:26:14 +0400

    feat: add modal

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,10 +15,12 @@ function App() {
     { id: 3, title: 'Java', body: 'Description 3' },
   ])
 
-  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [filter, setFilter] = useState({sort: '', query: ''});
+  const [modal, setModal] = useState(false);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   }
 
   const removePost = (post) => {
@@ -38,9 +42,14 @@ function App() {
   return (
     <>
     <div className="App">
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Create post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
       <Counter/>
       <ClassCounter/>
-      <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
       <PostFilter
         filter={filter}
