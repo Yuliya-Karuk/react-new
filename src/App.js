25183fb36b4466ee971748e4d8336952f751076b
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-12-11 19:39:06 +0400

    feat: add filter component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript 1', body: 'Description 1' },
-    { id: 2, title: 'Javascript 2', body: 'Description 2' },
-    { id: 3, title: 'Javascript 3', body: 'Description 3' },
+    { id: 1, title: 'Javascript ', body: 'Description 1' },
+    { id: 2, title: 'Python', body: 'Description 2' },
+    { id: 3, title: 'Java', body: 'Description 3' },
   ])
+
+  const [filter, setFilter] = useState({sort: '', query: ''})
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -22,16 +23,30 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
+  const sortedPosts = useMemo(() => {
+    console.log('отработала функция сортед постс')
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+  }, [filter.query, sortedPosts])
+
   return (
     <>
     <div className="App">
       <Counter/>
       <ClassCounter/>
       <PostForm create={createPost}/>
-      {posts.length !== 0
-        ?<PostList remove={removePost} posts={posts} title="Post`s list 1"/>
-        :<h1 style={{textAlign: 'center'}}>Posts are not found</h1>
-      }
+      <hr style={{margin: '15px 0'}}/>
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post`s list 1"/>
     </div>
     </>
   );
