aae055512b85c11fc1de1514cb736e27435f4ae4
Yuliya-Karuk <karuk.yuliya@gmail.com>
2023-12-12 19:36:26 +0400

    feat: add function for creating routes for different path

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,14 +4,14 @@ import Posts from "../pages/Posts";
 import About from "../pages/About";
 import Error from "../pages/Error";
 import PostIdPage from '../pages/PostIdPage';
+import { routes } from '../router';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path='/about' element={<About />} />
-      <Route exact  path='/posts' element={<Posts />} />
-      <Route path='/error' element={<Error />} />
-      <Route exact path='/posts/:id' element={<PostIdPage />} />
+      {routes.map(route =>
+        <Route exact={route.exact} path={route.path} element={route.component} />
+      )}
       <Route path="/*" element={<Navigate to="/posts" replace />} />
     </Routes>
   );
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Posts from "../pages/Posts"
+import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
+
+export const routes = [
+  {path: '/about', component: <About />, exact: true},
+  {path: '/posts', component: <Posts />, exact: true},
+  {path: '/posts/:id', component: <PostIdPage />, exact: true},
+]
